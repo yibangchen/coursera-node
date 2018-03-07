bbb0666a1bbed746c6a4e2d9b3f2dc0d502230c8
Yibang Chen <ychen622@gmail.com>
2018-03-06 17:48:22 -0800

    https setup

--- confusion-server/app.js
+++ confusion-server/app.js
@@ -37,6 +37,16 @@ db.once('open', () => {
 
 var app = express();
 
+// redirect all traffics to https
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();  // why return??
+  }
+  else {
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+  }
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
