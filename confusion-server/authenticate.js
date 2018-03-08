1358dd34fa08a65cab65f698593c203a2c13ff97
Yibang Chen <ychen622@gmail.com>
2018-03-07 21:05:23 -0800

    facebook login

--- confusion-server/authenticate.js
+++ confusion-server/authenticate.js
@@ -7,6 +7,7 @@ var User = require('./models/user');
 var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 var jwt = require('jsonwebtoken');
+var FacebookTokenStrategy = require('passport-facebook-token');
 
 var config = require('./config');
 
@@ -42,3 +43,43 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts,
 	}));
 
 exports.verifyUser = passport.authenticate('jwt', {session: false});
+
+exports.verifyAdmin = (req, res, next) => {
+	if (req.user.admin) next();
+	else {
+		var err = new Error('You are not authorized');
+		err.status = 403;
+		return next(err);
+	}
+};
+
+exports.facebookPassport = passport.use(new 
+	FacebookTokenStrategy({
+		clientID: config.facebook.clientId,
+		clientSecret: config.facebook.clientSecret
+	}, (accessToken, refreshToken, profile, done) => {
+		User.findOne({facebookId: profile.id}, (err, user) => {
+			if (err) {
+				return done(err, false);
+			}
+			if (!err && user !== null) { // user already registered
+				return done(null, user);
+			} else {
+				// create a new user
+				user = new User({ username: profile.displayName});
+				user.facebookId = profile.id;
+				user.firstname = profile.name.givenName;
+				user.lastname = profile.name.familyName;
+				user.save((err, user) => {
+					if (err)
+						return done(err, false);
+					else
+						return done(null, user);
+				});
+			}
+		});
+
+	})
+);
+
+
--- confusion-server/config.js
+++ confusion-server/config.js
@@ -1,4 +1,8 @@
 module.exports = {
 	'secretKey': '12345-27465-39408-77383',
-	'mongoUrl': 'mongodb://localhost:27017/conFusion'
+	'mongoUrl': 'mongodb://localhost:27017/conFusion',
+	'facebook': {
+		clientId: '382274532246409' ,
+		clientSecret: 'c7c67a3729915a9e39bd9f7bb01d584c'
+	}
 }
